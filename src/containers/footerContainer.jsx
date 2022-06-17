612737981862ccf91ed0d5f4837efa157b9ac3a2
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 15:28:13 +0000

    ✨feat: populated footer content

---
+++ src/containers/footerContainer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Footer from '../components/footer/footer'
+
+export const FooterContainer = () => {
+	return (
+		<Footer>
+            <Footer.Title>Questions? Contact Us</Footer.Title>
+            <Footer.Break />
+            <Footer.Row>
+                <Footer.Column>
+                    <Footer.Link href="#">FAQ</Footer.Link>
+                    <Footer.Link href="#">Investor Relations</Footer.Link>
+                    <Footer.Link href="#">Privacy</Footer.Link>
+                    <Footer.Link href="#">Speed Test</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href="#">Help Center</Footer.Link>
+                    <Footer.Link href="#">Jobs</Footer.Link>
+                    <Footer.Link href="#">Cookie Preferences</Footer.Link>
+                    <Footer.Link href="#">Legal Notices</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href="#">Account</Footer.Link>
+                    <Footer.Link href="#">Ways To Watch</Footer.Link>
+                    <Footer.Link href="#">Coorporate Information</Footer.Link>
+                    <Footer.Link href="#">Only on Netflix</Footer.Link>
+                </Footer.Column>
+                <Footer.Column>
+                    <Footer.Link href="#">Media Center</Footer.Link>
+                    <Footer.Link href="#">Terms of Use</Footer.Link>
+                    <Footer.Link href="#">Contact Us</Footer.Link>
+                </Footer.Column>
+            </Footer.Row>
+            <Footer.Break />
+            <Footer.Text> Netflix Ghana </Footer.Text>
+		</Footer>
+	)
+}
