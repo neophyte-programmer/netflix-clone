125a735c4bb38a0d1b94671de5cb3be9b0cfa961
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 12:12:04 +0000

    style: formatted code

--- src/components/jumbotron/jumbotron.jsx
+++ src/components/jumbotron/jumbotron.jsx
@@ -1,40 +1,43 @@
 import React from 'react'
-import {Inner, Container, Pane, Title, SubTitle, Image} from './styles/jumbotron'
+import {
+	Inner,
+	Item,
+	Container,
+	Pane,
+	Title,
+	SubTitle,
+	Image,
+} from './styles/jumbotron'
 
-const Jumbotron = ({ children, direction = "row", ...restProps }) => {
-  return (
-    <Inner direction={direction} >
-      {children}
-    </Inner>
-  )
-}
-
-const JumbotronContainer = ({children, ...restProps}) => {
-  return <Container {...restProps}>{children}</Container>
-}
-
-const JumbotronPane = ({children, ...restProps}) => {
-  return <Pane {...restProps}>{children}</Pane>
-}
-const JumbotronTitle = ({children, ...restProps}) => {
-  return <Title {...restProps}>{children}</Title>
-}
-const JumbotronSubTitle = ({children, ...restProps}) => {
-  return <SubTitle {...restProps}>{children}</SubTitle>
-}
-const JumbotronImage = ({ ...restProps}) => {
-  return <Image {...restProps}/>
+const Jumbotron = ({ children, direction = 'row', ...restProps }) => {
+	return (
+		<Item direction={direction} {...restProps}>
+			<Inner>{children}</Inner>
+		</Item>
+	)
 }
 
+const JumbotronContainer = ({ children, ...restProps }) => {
+	return <Container {...restProps}>{children}</Container>
+}
 
+const JumbotronPane = ({ children, ...restProps }) => {
+	return <Pane {...restProps}>{children}</Pane>
+}
+const JumbotronTitle = ({ children, ...restProps }) => {
+	return <Title {...restProps}>{children}</Title>
+}
+const JumbotronSubTitle = ({ children, ...restProps }) => {
+	return <SubTitle {...restProps}>{children}</SubTitle>
+}
+const JumbotronImage = ({ ...restProps }) => {
+	return <Image {...restProps} />
+}
 
 Jumbotron.Container = JumbotronContainer
 Jumbotron.Pane = JumbotronPane
 Jumbotron.Title = JumbotronTitle
 Jumbotron.SubTitle = JumbotronSubTitle
 Jumbotron.Image = JumbotronImage
-
-
-
 
 export default Jumbotron
