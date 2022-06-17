eb07494fb3f4998e4542d4d0de8e8a75c657fa9e
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 12:37:51 +0000

    perf: replaced raw code with component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react'
-import Jumbotron from './components/jumbotron/jumbotron'
-import jumbotronData from './fixtures/jumbo.json'
+import { JumboContainer } from './containers/jumbotronContainer'
 
 const App = () => {
-	return (
-		<div>
-			<Jumbotron.Container>
-				{/* Pass the data and direction from the json file to the component */}
-				{jumbotronData.map((item) => (
-					<Jumbotron key={item.id} direction={item.direction}>
-						<Jumbotron.Pane>
-							<Jumbotron.Title>{item.title}</Jumbotron.Title>
-							<Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-            </Jumbotron.Pane>
-            <Jumbotron.Pane>
-              <Jumbotron.Image src={item.image} alt={item.alt} />
-            </Jumbotron.Pane>
-					</Jumbotron>
-				))}
-			</Jumbotron.Container>
-		</div>
+  return (
+    <div>
+      <JumboContainer />
+    </div>
 	)
 }
 
