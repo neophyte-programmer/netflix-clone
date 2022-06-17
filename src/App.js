e3735d59de2112c714826eca2ad78274ad9d4266
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 15:27:23 +0000

    🔥refactor: called Footer component in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FooterContainer } from './containers/footerContainer'
 import {JumboContainer}  from './containers/jumbotronContainer'
 
 const App = () => {
   return (
     <div>
       <JumboContainer />
+      <FooterContainer />
     </div>
 	)
 }
