62a45b7eb0f2da982f420d09fc1e92dcbe25adce
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 17:23:12 +0000

    ✨feat!: accordion component

---
+++ src/containers/accordionContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import accordionData from '../fixtures/faqs.json'
+import Accordion from '../components/accordion/accordion'
+
+export const AccordionContainer = () => {
+	return (
+		<Accordion.Container>
+			<Accordion.Title> Frequently Asked Questions </Accordion.Title>
+			{accordionData.map((item) => (
+                <Accordion.Item key={item.id}>
+                    <Accordion.Header>{item.header}</Accordion.Header>
+                    <Accordion.Body>{item.body}</Accordion.Body>
+                </Accordion.Item>
+			))}
+		</Accordion.Container>
+	)
+}
