6a124f9a858843388cef649614c8b54e6cdea49b
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 15:11:15 +0000

    🎨style: footer styling

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components/macro'
+
+export const Container = styled.div`
+    display: flex;
+    padding: 70px 56px;
+    margin: auto;
+    max-width: 1000px;
+    flex-direction: column;
+
+    @media (max-width: 100px) {
+        padding: 70px 30px;
+    }
+`
+
+export const Row = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    grid-gap: 1rem;
+    
+    @media (max-width: 1000px) {
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+
+    }
+`
+
+export const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+`
+
+export const Link = styled.a`
+    color: #757575;
+    margin-bottom: 20px;
+    font-size: .9rem;
+    text-decoration: none;   
+`
+
+export const Break = styled.div`
+    flex-basis: 100%;
+    height: 0;
+`
+
+export const Title = styled.p`
+    font-size: 1rem;
+    color: #757575;
+    margin-bottom: 40px;
+`
+
+export const Text = styled.p`
+    font-size: .9rem;
+    color: #757575;
+    margin-bottom: 40px;
+`
+
