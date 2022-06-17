f13f0313401fef5e16281a32b49738fd661582ce
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 12:38:59 +0000

    perf: created jumbotron container component

---
+++ src/containers/jumbotronContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Jumbotron from '../components/jumbotron/jumbotron'
+import jumbotronData from '../fixtures/jumbo.json'
+
+export const JumboContainer = () => {
+	return (
+		<Jumbotron.Container>
+			{/* Pass the data and direction from the json file to the component */}
+			{jumbotronData.map((item) => (
+				<Jumbotron key={item.id} direction={item.direction}>
+					<Jumbotron.Pane>
+						<Jumbotron.Title>{item.title}</Jumbotron.Title>
+						<Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+					</Jumbotron.Pane>
+					<Jumbotron.Pane>
+						<Jumbotron.Image src={item.image} alt={item.alt} />
+					</Jumbotron.Pane>
+				</Jumbotron>
+			))}
+		</Jumbotron.Container>
+	)
+}
+
+
