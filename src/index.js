0ab934d8f2d4df06a1c1af26186d2e45c6b45137
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 12:11:03 +0000

    style: implemented global styles and normalize.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
+import { GlobalStyles } from './globalStyles'
+import 'normalize.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<App />
+		<>
+			<GlobalStyles />
+			<App />
+		</>
 	</React.StrictMode>
 )
