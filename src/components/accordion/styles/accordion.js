c35eb50dfd6843423ac2ab54e9717f2283829c4d
Nutifafa Attor <attorfafa@gmail.com>
2022-06-17 17:23:45 +0000

    🎨style: accordion style template

---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,32 @@
+import styled from "styled-components/macro";
+
+export const Container = styled.div`
+
+`
+
+export const Inner = styled.div`
+
+`
+
+export const Frame = styled.div`
+
+`
+
+export const Item = styled.div`
+
+`
+
+export const Title = styled.h1`
+
+`
+
+export const Header = styled.div`
+
+`
+
+export const Body = styled.div`
+
+`
+
+
+
