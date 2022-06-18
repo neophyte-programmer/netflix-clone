0d8e202e771af17e25cd72e97b300e24664cd88c
Nutifafa Attor <attorfafa@gmail.com>
2022-06-18 13:52:47 +0000

    ✨feat: setting up react router in app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React from 'react'
-import { FooterContainer } from './containers/footerContainer'
-import { JumboContainer } from './containers/jumbotronContainer'
-import { AccordionContainer } from './containers/accordionContainer'
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+} from 'react-router-dom'
+import * as ROUTES from './constants/routes'
+import Home from './pages/home'
+import Browse from './pages/browse'
+import SignIn from './pages/signin'
+import SignUp from './pages/signup'
+
+// The version 6 of react-router-dom introduced a new component called Routes which is an upgrade from Switch.
+// Also the new component introduced a new prop called element which is a React component.
+// The element prop is used to render the component that is being matched.
+// The Route component doesn't take any children.
+
 
 const App = () => {
-  return (
-    <div>
-      <JumboContainer />
-      <AccordionContainer />
-      <FooterContainer />
-    </div>
+	return (
+		<Router>
+			<Routes>
+				<Route exact path={ROUTES.HOME} element={<Home />} />
+
+				<Route exact path={ROUTES.BROWSE} element={<Browse />} />
+
+				<Route exact path={ROUTES.SIGN_UP} element={<SignUp />} />
+
+				<Route exact path={ROUTES.SIGN_IN} element={<SignIn />} />
+			</Routes>
+		</Router>
 	)
 }
 
