900194855fd91d27c05357d8d3442791cdad2cbe
Nutifafa Attor <attorfafa@gmail.com>
2022-06-18 15:46:51 +0000

    ✨feat: created header container

---
+++ src/containers/headerContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../components/header/header'
+import * as ROUTES from '../constants/routes'
+import logo from '../logo.svg'
+
+export const HeaderContainer = ({children}) => {
+    return (
+        <Header>
+            <Header.Frame>
+                <Header.Logo to={ROUTES.HOME} alt='netflix' src={logo} />
+                <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
+            </Header.Frame>
+            {children}
+        </Header>
+    )
+}
+
