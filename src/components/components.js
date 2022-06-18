4d787f6a2fa658f3eee8a42c4e43b0a09ffee96a
Nutifafa Attor <attorfafa@gmail.com>
2022-06-18 10:14:56 +0000

    🔥refactor: deleted redundant file

--- src/components/components.js
+++
@@ -1,4 +0,0 @@
-// This file will store the exports of all the components that will be used in the app
-// import Jumbotron from './jumbotron/jumbotron'
-
-export { default as Jumbotron }  from './jumbotron'  
