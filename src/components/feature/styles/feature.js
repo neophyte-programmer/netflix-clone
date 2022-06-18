cfd0e646ef398db7039ce23bd65717db85fba798
Nutifafa Attor <attorfafa@gmail.com>
2022-06-18 19:49:06 +0000

    🎨style: added padding for responsiveness

--- src/components/feature/styles/feature.js
+++ src/components/feature/styles/feature.js
@@ -1,34 +1,38 @@
-import styled from 'styled-components/macro';
+import styled from 'styled-components/macro'
 
 export const Inner = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-    justify-content: center;
-  text-align: center;
-    margin: 0 auto;
-    height: 100%;
-  `;
-  
-  export const Title = styled.h1`
-  color: white;
-  max-width: 640px;
-  font-size: 50px;
-  font-weight: 700;
-  margin: 0 auto;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+	margin: 0 auto;
+	height: 100%;
 
   @media (max-width: 600px) {
-    font-size: 28px;
+    padding: 0 20px;
   }
-`;
+`
+
+export const Title = styled.h1`
+	color: white;
+	max-width: 640px;
+	font-size: 50px;
+	font-weight: 700;
+	margin: 0 auto;
+
+	@media (max-width: 600px) {
+		font-size: 28px;
+	}
+`
 
 export const SubTitle = styled.h2`
-  color: white;
-  font-size: 26px;
-  font-weight: normal;
-  margin: 16px auto;
+	color: white;
+	font-size: 26px;
+	font-weight: normal;
+	margin: 16px auto;
 
-  @media (max-width: 600px) {
-    font-size: 18px;
-  }
-`;
+	@media (max-width: 600px) {
+		font-size: 18px;
+	}
+`
