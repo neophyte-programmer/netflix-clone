c92ff9989742cdefec906979a214190721e3dfd6
Nutifafa Attor <attorfafa@gmail.com>
2022-06-19 12:38:43 +0000

    ✨feat: included firebase context provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './app'
 import { GlobalStyles } from './globalStyles'
 import 'normalize.css'
+import firebase from './lib/firebase.prod.js'
+import { FirebaseContext } from './context/firebase'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<>
-			<GlobalStyles />
-			<App />
+			<FirebaseContext.Provider value={{ firebase }}>
+				<GlobalStyles />
+				<App />
+			</FirebaseContext.Provider>
 		</>
 	</React.StrictMode>
 )
