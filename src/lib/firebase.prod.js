dabe26ecd97df60afab4e32c6311324ab8d9dd9d
Nutifafa Attor <attorfafa@gmail.com>
2022-06-19 12:20:37 +0000

    ✨feat: seeded database without bugs

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,17 +1,25 @@
-import Firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
-
-// Seed the database
+import * as Firebase from 'firebase/app';
+import 'firebase/auth'
+import 'firebase/firestore'
+// import { seedDatabase } from '../seed'
 
 
 // Configure Firebase
-const config = {}
+const config = {
+    apiKey: 'AIzaSyCatg_LS7q5OFdXDjhYYdBSjzs6RVH2Cck',
+	authDomain: 'netflix-clone-f2579.firebaseapp.com',
+	projectId: 'netflix-clone-f2579',
+	storageBucket: 'netflix-clone-f2579.appspot.com',
+	messagingSenderId: '585476757133',
+	appId: '1:585476757133:web:a43f12e7f03295946474c4',
+}
 
 // Initialize Firebase
 const firebase = Firebase.initializeApp(config)
 
+// Seed the database once to avoid duplicate data
+// seedDatabase(firebase)
+
 
 // Export the database for use by the rest of the app
-export { firebase }
+export default firebase 
