45af25209ea2d75254d484eb56993f4763427073
Nutifafa Attor <attorfafa@gmail.com>
2022-06-20 22:16:54 +0000

    ✨feat: implemented redirect and protected routes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ import Home from './pages/home'
 import Browse from './pages/browse'
 import SignIn from './pages/signin'
 import SignUp from './pages/signup'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 
 const App = () => {
+	const user = null
 	return (
 		<Router>
 			<Switch>
-				<Route exact path={ROUTES.HOME} component={Home} />
+				{/* <Route exact path={ROUTES.SIGN_IN} component={SignIn} /> */}
+				<IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
+					<SignIn />
+				</IsUserRedirect>
 
-				<Route exact path={ROUTES.BROWSE} component={Browse} />
+				{/* <Route exact path={ROUTES.SIGN_UP} component={SignUp} /> */}
+				<IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact>
+					<SignUp />
+				</IsUserRedirect>
 
-				<Route exact path={ROUTES.SIGN_UP} component={SignUp} />
+				{/* <Route exact path={ROUTES.BROWSE} component={Browse} /> */}
+				<ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+					<Browse />
+				</ProtectedRoute>
 
-				<Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+				{/* <Route exact path={ROUTES.HOME} component={Home} /> */}
+				<IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
+					<Home />
+				</IsUserRedirect>
+
+
 			</Switch>
 		</Router>
 	)
