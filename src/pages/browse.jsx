b70488e29dec5700f94a7ed527c3ae644c1f000c
Nutifafa Attor <attorfafa@gmail.com>
2022-06-20 23:38:30 +0000

    ✨feat: set up usecontent to filter films n series

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import useContent from '../hooks/useContent'
 
 const Browse = () => {
+  // getting series and films
+  const { series } = useContent('series')
+  const { films } = useContent('films')
+  console.log(series)
+  console.log(films)
+
   return (
     <div>Browse</div>
   )
