30f2705d27500cccafcf5d12d9ed4753466cbf04
Nutifafa Attor <attorfafa@gmail.com>
2022-06-20 11:52:25 +0000

    ✨feat: created sign in page

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FooterContainer } from '../containers/footerContainer'
+import { HeaderContainer } from '../containers/headerContainer'
+import Form from '../components/form/form'
 
 const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [error, setError] = useState('')
+
+	// form validation
+  const handleSignIn = (e) => {
+    e.preventDefault()
+
+    // implement Firebase 
+  } 
+  // If password or email is empty, isInvalid will be true
+  const isInvalid = password === '' || email === ''
+
+	return (
+		<>
+			<HeaderContainer>
+				<Form>
+					<Form.Title>Sign In</Form.Title>
+					{error && <Form.Error>{error}</Form.Error>}
+
+					<Form.Base onSubmit={handleSignIn} method='POST'>
+            {/* When the value of input changes, set the email to the current value of the form */}
+						<Form.Input
+							placeholder='Email Address'
+							value={email}
+							onChange={({ target }) => setEmail(target.value)}
+            />
+            {/* When the value of input changes, set the password to the current value of the form */}
+						<Form.Input
+							placeholder='Password'
+							type='password'
+							autoComplete='off'
+							value={password}
+							onChange={({ target }) => setPassword(target.value)}
+            />
+            {/* Disable button if any of the inputs are not validated */}
+            <Form.Submit disabled={isInvalid} type="submit">Sign In</Form.Submit>
+          </Form.Base>
+          <Form.Text>New To Nutiflix? {' '}           
+          <Form.Link to='/signup'>Sign Up now</Form.Link>
+          </Form.Text>
+				</Form>
+			</HeaderContainer>
+			<FooterContainer> </FooterContainer>
+		</>
+	)
 }
 
 export default SignIn
