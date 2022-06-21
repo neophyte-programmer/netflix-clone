cafe105100742943269f2505acf36a0d32eb280e
Nutifafa Attor <attorfafa@gmail.com>
2022-06-21 16:57:11 +0000

    ✨feat: added more compound components

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,33 +1,122 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link as ReactRouterLink } from 'react-router-dom'
-import { Logo, ButtonLink, Background, Container} from './styles/header'
-
+import {
+	Logo,
+	ButtonLink,
+	Background,
+	Container,
+	Feature,
+	Text,
+	FeatureCallOut,
+	TextLink,
+	Group,
+	Profile,
+	Picture,
+	Dropdown,
+	Search,
+	SearchInput,
+	SearchIcon,
+	PlayButton,
+} from './styles/header'
 
 // bg prop for dynamic background
 const Header = ({ bg = true, children, ...restProps }) => {
-    // if the header takes a background, set the background, take restProps and pass children else just pass children as object
-    return bg ? <Background {...restProps}>{children}</Background> : children
+	// if the header takes a background, set the background, take restProps and pass children else just pass children as object
+	return bg ? <Background {...restProps}>{children}</Background> : children
 }
 
-const HeaderFrame = ({ children, ...restProps }) => { 
-    return <Container {...restProps}>{children}</Container>
+const HeaderFeature = ({ children, ...restProps }) => {
+	return <Feature {...restProps}>{children}</Feature>
 }
 
-const HeaderLogo = ({ to, ...restProps }) => { 
-    return (
-        <ReactRouterLink to={to}>
-            <Logo {...restProps} />
-        </ReactRouterLink>
-    )
+const HeaderFeatureCallOut = ({ children, ...restProps }) => {
+	return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
 }
 
-const HeaderButtonLink = ({ to, children, ...restProps }) => { 
-    return <ButtonLink to={to} {...restProps}>{children}</ButtonLink>
+const HeaderText = ({ children, ...restProps }) => {
+	return <Text {...restProps}>{children}</Text>
 }
 
+const HeaderTextLink = ({ children, ...restProps }) => {
+	return <TextLink {...restProps}>{children}</TextLink>
+}
 
+const HeaderFrame = ({ children, ...restProps }) => {
+	return <Container {...restProps}>{children}</Container>
+}
+
+const HeaderLogo = ({ to, ...restProps }) => {
+	return (
+		<ReactRouterLink to={to}>
+			<Logo {...restProps} />
+		</ReactRouterLink>
+	)
+}
+
+const HeaderButtonLink = ({ to, children, ...restProps }) => {
+	return (
+		<ButtonLink to={to} {...restProps}>
+			{children}
+		</ButtonLink>
+	)
+}
+
+const HeaderGroup = ({ to, children, ...restProps }) => {
+	return (
+		<Group to={to} {...restProps}>
+			{children}
+		</Group>
+	)
+}
+
+const HeaderProfile = ({ children, ...restProps }) => {
+	return <Profile {...restProps}>{children}</Profile>
+}
+
+const HeaderPicture = ({ src, ...restProps }) => {
+	return <Picture src={`/images/users/${src}.png`} {...restProps} />
+}
+
+const HeaderDropdown = ({ children, ...restProps }) => {
+	return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
+const HeaderSearch = ({ searchTerm, setSearchTerm, ...restProps }) => {
+	const [searchActive, setSearchActive] = useState(false)
+
+	return (
+		<Search {...restProps}>
+			<SearchIcon
+				onClick={() => setSearchActive((searchActive) => !searchActive)}
+			>
+				<img src='/images/icons/search.png' alt='search' />
+			</SearchIcon>
+			<SearchInput
+				value={searchTerm}
+				onChange={({ target }) => setSearchTerm(target.value)}
+				placeholder='Search films and series'
+				active={searchActive}
+			/>
+		</Search>
+	)
+}
+
+const HeaderPlayButton = ({ children, ...restProps }) => {
+	<PlayButton {...restProps}>{children}</PlayButton>
+}
+
+Header.Feature = HeaderFeature
+Header.Text = HeaderText
+Header.TextLink = HeaderTextLink
+Header.FeatureCallOut = HeaderFeatureCallOut
 Header.Frame = HeaderFrame
 Header.Logo = HeaderLogo
 Header.ButtonLink = HeaderButtonLink
+Header.Group = HeaderGroup
+Header.Profile = HeaderProfile
+Header.Picture = HeaderPicture
+Header.Dropdown = HeaderDropdown
+Header.Search = HeaderSearch
+Header.PlayButton = HeaderPlayButton
 
 export default Header
