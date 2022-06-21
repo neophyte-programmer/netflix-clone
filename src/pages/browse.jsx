fa5cc3311404306acbe4a6698b3ebf39ed50b8fa
Nutifafa Attor <attorfafa@gmail.com>
2022-06-21 09:23:37 +0000

    ✨feat: updated browse page

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { BrowseContainer } from '../containers/browseContainer'
 import useContent from '../hooks/useContent'
+import selectionFilter from '../utils/selectionFilter'
 
 const Browse = () => {
   // getting series and films
   const { series } = useContent('series')
   const { films } = useContent('films')
-  console.log(series)
-  console.log(films)
+
+  // getting the slides
+  const slides = selectionFilter({ series, films })
 
   return (
-    <div>Browse</div>
+    <BrowseContainer slides={slides} />
   )
 }
 
