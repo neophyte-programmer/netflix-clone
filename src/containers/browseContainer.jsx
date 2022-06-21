4f72627898ef33c6bf18eb6dbb35be1554c63fe4
Nutifafa Attor <attorfafa@gmail.com>
2022-06-21 09:37:39 +0000

    ✨feat: update browse container

--- src/containers/browseContainer.jsx
+++ src/containers/browseContainer.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import { SelectProfileContainer } from './profileContainer'
 import { FirebaseContext } from '../context/firebase'
 import { useContext, useState, useEffect } from 'react'
+import Loading from '../components/loading/loading'
 
 export const BrowseContainer = ({ slides }) => {
-    const [profile, setProfile] = useState({})
-    const [loading, setLoading] = useState(true)
-    const { firebase } = useContext(FirebaseContext)
-    const user = firebase.auth().currentUser || {}
+	const [profile, setProfile] = useState({})
+	const [loading, setLoading] = useState(true)
+	const { firebase } = useContext(FirebaseContext)
+	const user = firebase.auth().currentUser || {}
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 3000);
-    }, [profile.displayName])
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false)
+		}, 3000)
+	}, [profile.displayName])
 
-	return <SelectProfileContainer user={user} setProfile={setProfile} />
+	return profile.displayName ? (
+		loading ? (
+			<Loading src={user.photoURL} />
+		) : null
+	) : (
+		<SelectProfileContainer user={user} setProfile={setProfile} />
+	)
 }
