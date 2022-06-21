1bce5cd42c5f83dae5f38cddf7080d2b00b938f3
Nutifafa Attor <attorfafa@gmail.com>
2022-06-21 08:12:55 +0000

    🎨style: formatted code

--- src/hooks/useContent.js
+++ src/hooks/useContent.js
@@ -6,28 +6,28 @@ const useContent = (target) => {
 	const [content, setContent] = useState([])
 	const { firebase } = useContext(FirebaseContext)
 
-    useEffect(() => {
-        // use firebase and firestore to access collection
+	useEffect(() => {
+		// use firebase and firestore to access collection
 		firebase
 			.firestore()
-            .collection(target)
-            //here we get the collection
+			.collection(target)
+			//here we get the collection
 			.get()
 			.then((snapshot) => {
 				const allContent = snapshot.docs.map((contentObj) => ({
-                    ...contentObj.data(),
-                    // here we get the id of the content so we can use it as a key
-                    documentID: contentObj.id,
-                    
-                }))
-                
-                setContent(allContent)
-            }).catch((error) => {
-                console.log(error.message)
-            })
-    }, [])
-    
-    return { [target]: content}
+					...contentObj.data(),
+					// here we get the id of the content so we can use it as a key
+					documentID: contentObj.id,
+				}))
+
+				setContent(allContent)
+			})
+			.catch((error) => {
+				console.log(error.message)
+			})
+	}, [])
+
+	return { [target]: content }
 }
 
 export default useContent
